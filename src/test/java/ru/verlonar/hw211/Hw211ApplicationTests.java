6b87dea084b91a1954108cc69cd83727499ce3a8
Verlonar <garvlok@gmail.com>
2022-06-02 19:27:37 +0300

    Initial commit

---
+++ src/test/java/ru/verlonar/hw211/Hw211ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.verlonar.hw211;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
